9b10bad76819e3ea9e2d7a216dc0837224ff041c
Neptune2k21 <116343729+Neptune2k21@users.noreply.github.com>
2025-04-09 14:15:33 +0200

    Twig & Templates

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,14 +2,23 @@
 namespace App\Controller;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class MainController
+class MainController extends AbstractController
 {
     #[Route]
     public function HomePage() : Response 
     {
-        return new Response(
-            '<html><body>Welcome to the Home Page!</body></html>'
-        );
+        $starShipCount = 457;
+        $myShip = [
+            'name' => 'USS LeafyCruiser (NCC-0001)',
+            'class' => 'Garden',
+            'captain' => 'Jean-Luc Pickles',
+            'status' => 'under construction',
+        ];
+       return $this->render("main/homepage.html.twig", [
+            'numberOfStarships' => $starShipCount,
+            'myShip' => $myShip,
+        ]);
     }
 }
