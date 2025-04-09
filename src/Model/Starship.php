4ec83fd2d902010eaf57c1ea4658b42db643068a
Neptune2k21 <116343729+Neptune2k21@users.noreply.github.com>
2025-04-09 15:07:54 +0200

    Création d'une API JSON

---
+++ src/Model/Starship.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Model;
+
+class Starship{
+    public function __construct(
+        private int $id,
+        private string $name,
+        private string $class,
+        private string $captain,
+        private string $status
+    ) {}
+    public function getId(): int
+    {
+        return $this->id;
+    }
+    public function getName(): string
+    {
+        return $this->name;
+    }
+    public function getClass(): string
+    {
+        return $this->class;
+    }
+}
